89cb3f9a0ae9a72b2bfa80a1d7ee49ea6397fecf
ephraim <ephraims.818@gmail.com>
2025-08-28 12:39:39 +0600

    JS--COMPLETE-JS---ADVANCED-DOM-AND-EVENTS - #BANKIST - 01. Project Overview

---
+++ starter copy/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+///////////////////////////////////////
+/* ******************************************** */
+/*ANCHOR:                Modal Elements                */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                     Modal                    */
+
+const openModal = function (event) {
+  event.preventDefault(event);
+
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                  Modal Buttons                  */
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/* ******************************************** */
