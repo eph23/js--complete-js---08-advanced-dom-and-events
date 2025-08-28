303d46898f9cb0e180d5bed01e3ad1c84f3032b2
ephraim <ephraims.818@gmail.com>
2025-08-29 01:07:06 +0600

    JS--COMPLETE-JS---ADVANCED-DOM-AND-EVENTS - #BANKIST - 02. Implementing Smooth Scrolling

---
+++ #Bankist - 02. Implementing Smooth Scrolling/script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+///////////////////////////////////////
+/* ******************************************** */
+/*ANCHOR:                Elements                */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                     Modal                    */
+
+const openModal = function (event) {
+  event.preventDefault(event);
+
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                  Modal Buttons                  */
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                 Smooth Scroll                 */
+
+btnScrollTo.addEventListener('click', function (event) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(event.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+/* ******************************************** */
