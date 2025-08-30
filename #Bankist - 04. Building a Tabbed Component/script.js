63e31b0b7c6dfde6f7fe362fb0583e676d7bd7f6
ephraim <ephraims.818@gmail.com>
2025-08-31 02:54:14 +0600

    JS--COMPLETE-JS---ADVANCED-DOM-AND-EVENTS - #BANKIST - 04. Building a Tabbed Component

---
+++ #Bankist - 04. Building a Tabbed Component/script.js
@@ -0,0 +1,114 @@
+'use strict';
+
+///////////////////////////////////////
+/* ******************************************** */
+/*ANCHOR:                Elements                */
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                     Modal                    */
+
+const openModal = function (event) {
+  event.preventDefault(event);
+
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                  Modal Buttons                  */
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR:                 Smooth Scroll                 */
+
+btnScrollTo.addEventListener('click', function (event) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(event.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR                PAGE NAVIGATION               */
+
+document
+  .querySelector('.nav__links')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (event.target.classList.contains('nav__link')) {
+      const id = event.target.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+/* ******************************************** */
+
+/* ******************************************** */
+/*ANCHOR               TABBED COMPONENT               */
+
+tabsContainer.addEventListener('click', function (event) {
+  const clicked = event.target.closest('.operations__tab');
+
+  if (!clicked) {
+    return;
+  }
+
+  tabs.forEach(tab => {
+    tab.classList.remove('operations__tab--active');
+  });
+
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+/* ******************************************** */
